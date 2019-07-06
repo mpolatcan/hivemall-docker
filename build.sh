03ee4ff9d4fe5298feb6634f17f42513292451a6
Mutlu Polatcan <mutlu.polatcan@cci.com.tr>
2019-07-06 23:29:58 +0300

    Dockerfile entrypoints updated

---
+++ build.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+HIVEMALL_VERSIONS=(
+    "0.5.2"
+    "0.5.0"
+)
+
+HIVE_VERSIONS=(
+    "3.1.1"
+    "2.3.5"
+    "1.2.2"
+)
+
+HADOOP_VERSIONS=(
+    "3.2.0"
+    "3.1.2"
+    "3.0.3"
+    "2.9.1"
+    "2.8.4"
+    "2.7.6"
+)
+
+DISTS=(
+    "alpine"
+    "ubuntu"
+)
+
+# $1: DIST
+# $2: HIVEMALL_VERSION
+# $3: HIVE_VERSION
+# $4: HADOOP_VERSION
+function build_image() {
+    sudo docker build -q -t mpolatcan/hivemall:$1-$2-hive-$3-hadoop-$4 --build-arg HIVE_VERSION=$3 --build-arg HADOOP_VERSION=$4 ./$1/
+	sudo docker push mpolatcan/hivemall:$1-$2-hive-$3-hadoop-$4
+	sudo docker rmi $(sudo docker images -q)
+}
+
+for HIVEMALL_VERSION in ${HIVEMALL_VERSIONS[@]}; do
+    for HIVE_VERSION in ${HIVE_VERSIONS[@]}; do
+        for HADOOP_VERSION in ${HADOOP_VERSIONS[@]}; do
+            for DIST in ${DISTS[@]}; do
+                build_image $DIST $HIVEMALL_VERSION $HIVE_VERSION $HADOOP_VERSION
+            done
+        done
+    done
+done
